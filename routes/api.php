e695c072429a8d0e9cdf62d09f2f55dbc40ace35
Maszerus <p.k.rozmanowski@gmail.com>
2023-05-20 20:45:55 +0200

    routing

--- routes/api.php
+++ routes/api.php
@@ -4,29 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonController;
 
-Route::get('/all', [PersonController::class, 'index']);
-Route::get('/{id}', [PersonController::class, 'show']);
-Route::post('/store', [PersonController::class, 'store']);
-Route::put('/{id}', [PersonController::class, 'update']);
-Route::delete('/destroy/{id}', [PersonController::class, 'destroy']);
-
-
-// Route::get('/people', [PersonController::class, 'index']);
-// Route::get('/people/{id}', [PersonController::class, 'show']);
-// Route::post('/people', [PersonController::class, 'store']);
-// Route::put('/people/{id}', [PersonController::class, 'update']);
-// Route::delete('/people/{id}', [PersonController::class, 'destroy']);
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::get('/rozmanowski/53873/people/all', [PersonController::class, 'index']);
+Route::get('/rozmanowski/53873/people/{id}', [PersonController::class, 'show']);
+Route::post('/rozmanowski/53873/people/store', [PersonController::class, 'store']);
+Route::put('/rozmanowski/53873/people/{id}', [PersonController::class, 'update']);
+Route::delete('/rozmanowski/53873/people/destroy/{id}', [PersonController::class, 'destroy']);
